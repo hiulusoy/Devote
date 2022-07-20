c4ee84d784f1a14869e2b5864fb27f5e9172beb6
hiusanmaz <hiusanmaz34@gmail.com>
2022-07-20 09:25:49 +0300

    Initial Commit

---
+++ Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  Devote
+//
+//  Created by Halil Usanmaz on 20.07.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}

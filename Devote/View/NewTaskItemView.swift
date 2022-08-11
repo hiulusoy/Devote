f41485fcca1679aff5b57974f628195077129d4e
hiusanmaz <hiusanmaz34@gmail.com>
2022-08-11 17:19:57 +0300

    Completed Devote App

--- Devote/View/NewTaskItemView.swift
+++ Devote/View/NewTaskItemView.swift
@@ -57,7 +57,7 @@ struct NewTaskItemView: View {
                 Button(action: {
                     addItem()
                     playSound(sound: "sound-ding", type: "mp3")
-                    feedback.notificationOccurred(.)
+                    feedback.notificationOccurred(.success)
                 },label: {
                     Spacer()
                     Text("SAVE")
